a7d24cc089c68d910819c1e32e0b49b1c4e474a8
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-07-02 10:47:34 +0700

    remove: unused and duplicate import

--- lib/home/homepage_content.dart
+++ lib/home/homepage_content.dart
@@ -3,18 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pitstop/home/bloc/user_bloc.dart';
 import 'package:pitstop/home/bloc/user_state.dart';
 import 'package:pitstop/data/api/customer/customer_service.dart';
-import 'package:pitstop/home/booking_page.dart'; // Ensure this path is correct
-
-import 'package:pitstop/data/model/booking/booking_model.dart';
-import 'package:pitstop/data/model/service/service_model.dart';
-import 'package:pitstop/data/model/mechanic/mechanic_model.dart';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pitstop/home/bloc/user_bloc.dart';
-import 'package:pitstop/home/bloc/user_state.dart';
-import 'package:pitstop/data/api/customer/customer_service.dart';
-import 'package:pitstop/home/booking_page.dart'; // Ensure this path is correct
 
 import 'package:pitstop/data/model/booking/booking_model.dart';
 import 'package:pitstop/data/model/service/service_model.dart';
